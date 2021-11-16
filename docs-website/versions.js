cf71c4b29058a16ff897c3a14cbccc4577d62e0d
Derek Moore <derek.moore@dell.com>
2021-11-15 20:46:21 -0600

    add versions.js and branches with version substitution
    Signed-off-by: Derek Moore <derek.moore@dell.com>

---
+++ docs-website/versions.js
@@ -0,0 +1,21 @@
+const versions = Object.freeze({
+  "pravega": "0.10.1",
+  "pravega-branch": "r0.10",
+  "pravega-operator": "0.5.5",
+  "cert-manager": "1.5.4",
+  "zookeeper-operator-chart": "0.2.13",
+  "zookeeper-chart": "0.2.13",
+  "bookkeeper-operator-chart": "0.2.1",
+  "bookkeeper-chart": "0.10.1",
+  "pravega-operator-chart": "0.6.1",
+  "pravega-chart": "0.10.2",
+  "pravega-samples-branch": "r0.8",
+  "flink-connectors": "0.9.0",
+  "flink": "1.11.2",
+  "flink-minor": "1.11",
+  "scala": "2.12",
+  "spark-connectors": "0.9.0",
+  "": "",
+});
+
+export default versions;
