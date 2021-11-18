b48f3ae7a1fa3fb08ded04539251f75fe6dfa6b3
Derek Moore <derek.moore@dell.com>
2021-11-18 14:11:33 -0600

    update Pravega Samples and Flink & Spark Connectors versions
    Signed-off-by: Derek Moore <derek.moore@dell.com>

--- docs-website/versions.js
+++ docs-website/versions.js
@@ -9,12 +9,12 @@ const versions = Object.freeze({
   "bookkeeper-chart": "0.10.1",
   "pravega-operator-chart": "0.6.1",
   "pravega-chart": "0.10.2",
-  "pravega-samples-branch": "r0.8",
-  "flink-connectors": "0.9.0",
-  "flink": "1.11.2",
-  "flink-minor": "1.11",
+  "pravega-samples-branch": "r0.10",
+  "flink-connectors": "0.10.1",
+  "flink": "1.12.4",
+  "flink-minor": "1.12",
   "scala": "2.12",
-  "spark-connectors": "0.9.0",
+  "spark-connectors": "0.10.1",
   "": "",
 });
 
